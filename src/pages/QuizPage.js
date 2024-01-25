2d6492faa6dee1ff5d5dfe2120dfedefbe54028a
Muhammad Bilal Rizvi <bilal742742@gmail.com>
2024-01-26 00:23:52 +0500

    changes pushed

--- src/pages/QuizPage.js
+++ src/pages/QuizPage.js
@@ -7,22 +7,35 @@ import { IoStarOutline } from "react-icons/io5";
 const Quiz = () => {
   const [questionNum, setQuestionNum] = useState(0);
   const [CountCorrectQuestion, setCountCorrectQuestion] = useState(0);
-
-  const maxScore = (totalQues, correctQues, attemptedQues) => {};
+  const [selectedOption, setSelectedOption] = useState('');
+  const maxScore = (totalQues, correctQues, attemptedQues) => { };
 
   const onNext = () => {
     const cuurentQuesNumber = questionNum + 1 + 1;
     console.log(jsonData.length);
     console.log(cuurentQuesNumber);
+    setSelectedOption('');
 
     if (jsonData.length >= cuurentQuesNumber) {
       setQuestionNum(questionNum + 1);
     }
   };
+
+  const onOptionSelecton = (value) => {
+    setSelectedOption(value);
+    if (jsonData[questionNum].correct_answer === value) {
+      setCountCorrectQuestion(CountCorrectQuestion + 1);
+    }
+  }
+
+  const options = [...jsonData[questionNum].incorrect_answers, jsonData[questionNum].correct_answer]
   // console.log(decodeURI(jsonData[0].category))
   // console.log(uriToJSON(JSON.stringify(jsonData) ))
+  console.log({ CountCorrectQuestion })
+
   return (
     <div className="p-8 QuestionMainDiv my-50">
+      <div style={{ width: `${((questionNum + 1) / jsonData.length) * 100}%`, backgroundColor: "black", height: "5px" }}></div>
       <div className="QuestionNumDiv">
         <h1 className="text-2xl font-bold">
           {" "}
@@ -41,8 +54,8 @@ const Quiz = () => {
           {jsonData[questionNum].difficulty === "easy"
             ? <IoStarSharp />
             : jsonData[questionNum].difficulty == "medium"
-            ?  <div className="flex flex-row"><IoStarSharp /><IoStarSharp /></div>
-            : <div className="flex flex-row"><IoStarSharp /><IoStarSharp /><IoStarSharp /></div>}
+              ? <div className="flex flex-row"><IoStarSharp /><IoStarSharp /></div>
+              : <div className="flex flex-row"><IoStarSharp /><IoStarSharp /><IoStarSharp /></div>}
         </h3>
       </div>
       <div className="QuestionDiv">
@@ -54,23 +67,15 @@ const Quiz = () => {
         </h1>
       </div>
       <div className="flex flex-row flex-wrap justify-between mt-6 QuestionMcqsDiv gap-y-10">
-        <button className="border-2 border-black border-solid w-[45%] bg-gray-200 font-bold rounded">
-          {/* MCQS No: One */}
-          {decodeURIComponent(jsonData[questionNum].correct_answer)}
-        </button>
-        <button className="border-2 border-black border-solid w-[45%] bg-gray-200 font-bold rounded">
-          {/* MCQS No: Two */}
-          {decodeURIComponent(jsonData[questionNum].incorrect_answers[0])}
-        </button>
-        <button className="border-2 border-black border-solid w-[45%] bg-gray-200 font-bold rounded">
-          {/* MCQS No: Three */}
-          {decodeURIComponent(jsonData[questionNum].incorrect_answers[1])}
-        </button>
-        <button className="border-2 border-black border-solid w-[45%] bg-gray-200 font-bold rounded">
-          {/* MCQS No: Four */}
-          {decodeURIComponent(jsonData[questionNum].incorrect_answers[2])}
-        </button>
+        {options.map((op) => <button onClick={() => { onOptionSelecton(op) }} className="border-2 border-black border-solid w-[45%] bg-gray-200 font-bold rounded">
+          {decodeURIComponent(op)}
+        </button>)}
       </div>
+
+      {selectedOption && 
+      <>
+      <div className="flex flex-row flex-wrap justify-center mt-10 QuestionScoreBarDiv">{selectedOption === jsonData[questionNum].correct_answer ? "Correct" : "Sorry"} </div>
+      
       <div className="flex flex-row flex-wrap justify-center mt-10 QuestionScoreBarDiv">
         <button
           onClick={onNext}
@@ -79,14 +84,15 @@ const Quiz = () => {
           Next
         </button>
       </div>
-      <div className="bottomScore mt-60">
-          <div className="flex flex-row flex-wrap justify-between scoreMention">
-            <p>Score: 67%</p>
-            <p>Max Score: 75%</p>
-          </div>
-          <div className="h-8 bg-gray-200 border-2 border-black border-solid rounded colorMention ">
+      </>
+      }      <div className="bottomScore mt-60">
+        <div className="flex flex-row flex-wrap justify-between scoreMention">
+          <p>Score: 67%</p>
+          <p>Max Score: 75%</p>
+        </div>
+        <div className="h-8 bg-gray-200 border-2 border-black border-solid rounded colorMention ">
 
-          </div>
+        </div>
       </div>
     </div>
   );
