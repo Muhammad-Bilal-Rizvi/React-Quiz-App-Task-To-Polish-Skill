b7475fd61c70f20f1618ad23f3383664935372fa
Muhammad Bilal Rizvi <73776975+Muhammad-Bilal-Rizvi@users.noreply.github.com>
2024-01-25 14:47:26 +0500

    decodeURLComponent add
    little problem in MCQS index Like we have two options True and False

--- src/pages/QuizPage.js
+++ src/pages/QuizPage.js
@@ -28,7 +28,7 @@ const Quiz = () => {
           {" "}
           Question {questionNum + 1} of 20
         </h1>
-        <h3>{jsonData[questionNum].category}</h3>
+        <h3>{decodeURIComponent(jsonData[questionNum].category)}</h3>
 
         {/* <h3>
           {jsonData[questionNum].difficulty === "easy"
@@ -47,23 +47,28 @@ const Quiz = () => {
       </div>
       <div className="QuestionDiv">
         <h1 className="mt-6 text-lg font-medium">
-          At the start of a standard game of the monopoly, if you through a
+          {/* At the start of a standard game of the monopoly, if you through a
           double six, which square would you land on?
-          <IoStarOutline />
+          <IoStarOutline /> */}
+          {decodeURIComponent(jsonData[questionNum].question)}
         </h1>
       </div>
       <div className="flex flex-row flex-wrap justify-between mt-6 QuestionMcqsDiv gap-y-10">
         <button className="border-2 border-black border-solid w-[45%] bg-gray-200 font-bold rounded">
-          MCQS No: One
+          {/* MCQS No: One */}
+          {decodeURIComponent(jsonData[questionNum].correct_answer)}
         </button>
         <button className="border-2 border-black border-solid w-[45%] bg-gray-200 font-bold rounded">
-          MCQS No: Two
+          {/* MCQS No: Two */}
+          {decodeURIComponent(jsonData[questionNum].incorrect_answers[0])}
         </button>
         <button className="border-2 border-black border-solid w-[45%] bg-gray-200 font-bold rounded">
-          MCQS No: Three
+          {/* MCQS No: Three */}
+          {decodeURIComponent(jsonData[questionNum].incorrect_answers[1])}
         </button>
         <button className="border-2 border-black border-solid w-[45%] bg-gray-200 font-bold rounded">
-          MCQS No: Four
+          {/* MCQS No: Four */}
+          {decodeURIComponent(jsonData[questionNum].incorrect_answers[2])}
         </button>
       </div>
       <div className="flex flex-row flex-wrap justify-center mt-10 QuestionScoreBarDiv">
@@ -96,3 +101,12 @@ export default Quiz;
 {/* <button className="w-[45%] py-2 px-4 bg-gray-200 text-slate-600 font-semibold rounded-lg shadow-md hover:bg-black focus:outline-none focus:ring-2 focus:text-white focus:ring-opacity-75">
   MCQS No: Three
 </button>; */}
+
+// {
+//   "category": "Animals",
+//   "type": "boolean",
+//   "difficulty": "easy",
+//   "question": "Kangaroos%20keep%20food%20in%20their%20pouches%20next%20to%20their%20children.",
+//   "correct_answer": "False",
+//   "incorrect_answers": ["True","First","Second"] // changes add - "First","Second"
+// }
