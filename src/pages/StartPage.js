8ff09549c92f990c97183008e7a2e291e05d6d97
Muhammad Bilal Rizvi <73776975+Muhammad-Bilal-Rizvi@users.noreply.github.com>
2024-01-25 15:03:33 +0500

    remember add two incorrect answer

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -8,12 +8,12 @@ const StartPage = () => {
   return (
     <div>
       <div>
-        <img src={ExpertizoLogo} alt="ExpertizoLogo" class="SetLogoImg" />
+        <img src={ExpertizoLogo} alt="ExpertizoLogo" className="SetLogoImg" />
       </div>
       <div className="flex justify-center">
         <button
           onClick={() => navigate("/quiz")}
-          className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+          className="px-4 py-2 font-semibold text-white bg-blue-500 rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
         >
           Quiz Start
         </button>
